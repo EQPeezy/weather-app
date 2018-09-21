8981396d4672f2447746a85325deefbd6325814e
unknown <blesbparker@gmail.com>
2018-09-21 15:45:28 -0400

    Skeleton Built

--- src/App.js
+++ src/App.js
@@ -57,6 +57,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
+        <h1></h1>
         <Titles />
         <Form getWeather={this.getWeather} />
         <Weather
